061e9613dfafb0381f09ebff65a5700aea8fd3b2
indiflex <indiflex1@gmail.com>
2019-02-08 17:55:26 +0900

    req & res

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, g, request, Response, make_response
 
 app = Flask(__name__)
+app.debug = True
+
+@app.route('/rp')
+def rp():
+    # q = request.args.get('q')
+    q = request.args.getlist('q')
+    return "q= %s" % str(q)
+
+@app.route('/test_wsgi')
+def wsgi_test():
+    def application(environ, start_response):
+        body = 'The request method was %s' % environ['REQUEST_METHOD']
+        headers = [('Content-Type', 'text/plain'),
+                   ('Content-Length', str(len(body)))]
+        start_response('200 OK', headers)
+        return [body]
+
+    return make_response(application)
+
+@app.route('/res1')
+def res1():
+    custom_res = Response("Custom Response", 201, {'test': 'ttt'})
+    return make_response(custom_res)
+
+
+# @app.before_request
+# def before_request():
+#     print("before_request!!!")
+#     g.str = "한글"
+
+@app.route("/gg")
+def helloworld2():
+    return "Hello World!" + getattr(g, 'str', '111')
 
 @app.route("/")
 def helloworld():
-    return "Hello Flask World!"
+    return "Hello Flask World!!"
