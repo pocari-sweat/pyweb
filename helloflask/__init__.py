1e11e5b4f8687ac61173b110dffb683f8ea77ef7
indiflex <indiflex1@gmail.com>
2019-02-14 12:02:25 +0900

    modal

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,18 +1,31 @@
 from flask import Flask, g, request, Response, make_response
-from flask import session, render_template, Markup
+from flask import session, render_template, Markup, url_for
 from datetime import date, datetime, timedelta
 import os
-# from utils.a import ttt
 
 app = Flask(__name__)
 app.debug = True
-# app.jinja_env.trim_blocks = True
+app.jinja_env.trim_blocks = True
 
 app.config.update(
 	SECRET_KEY='X1243yRH!mMwf',
 	SESSION_COOKIE_NAME='pyweb_flask_session',
 	PERMANENT_SESSION_LIFETIME=timedelta(31)      # 31 days
 )
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
 class FormInput:
     def __init__(self, id='', name='', value='', checked='', text='', type='text'):
@@ -197,6 +210,22 @@ def helloworld2():
 #     return "Hello Flask World!!"
 
 
+@app.before_first_request
+def beforeFirst():
+    print(">> before_first_request!!")
+
+
+@app.after_request
+def afterReq(response):
+    print(">> after_request!!")
+    return response
+
+
 @app.teardown_request
-def teardown(exception):
-    print(">>> teardown!!", exception)
+def teardown_request(exception):
+    print(">>> teardown request!!", exception)
+
+
+@app.teardown_appcontext
+def teardown_context(exception):
+    print(">>> teardown context!!", exception)
