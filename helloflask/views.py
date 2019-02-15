28b61447f2460f108ab8e5079151382f65899ac1
indiflex <indiflex1@gmail.com>
2019-02-15 17:33:43 +0900

    userlist update & delete

--- helloflask/views.py
+++ helloflask/views.py
@@ -9,12 +9,21 @@ from helloflask.models import User
 @app.route('/')
 def idx():
     try:
-        u = User('abc@efg.com', 'hong')
-        db_session.add(u)
+        # u = User('abc@efg.com', 'hong')
+        # db_session.add(u)
+        u = User.query.filter(User.id == 10).first()
+        print("user.id=", u.id)
+        db_session.delete(u)
+        # u.email = 'indiflex1@gmail.com'
+        # db_session.add(u)
+
         db_session.commit()
 
         ret = "aaa"
-        ret = User.query.all()
+        # ret = User.query.all()
+        ret = User.query.filter(User.id > 5)
+        for user in ret:   # select * from Follower where user = 6
+            user.followers = Follower.query.filter(user == user.id)
         
     except SQLAlchemyError as sqlerr:
         db_session.rollback()
